c1287a1dbe025b7cfd9e85636e2308ff38bc1bcd
romashkarrr <r0man.kish999@gmail.com>
2024-06-05 13:55:28 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by romashka on 05.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
